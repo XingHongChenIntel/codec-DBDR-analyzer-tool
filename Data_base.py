b16a6210b4ec932a13eeb7972076cbeef54e8cc1
chandlerbingbing <327269655@qq.com>
2019-09-04 15:53:30 +0800

    review code structure

---
+++ Data_base.py
@@ -0,0 +1,68 @@
+import pickle
+import os
+from Data_struct import Line, LineContain, CaseDate
+
+
+class Database:
+    def __init__(self, datapath):
+        self.datapath = datapath
+        self.data = self.deserialize_data()
+        self.temp_case = None
+
+    def is_data_exist(self):
+        return os.path.exists(self.datapath)
+
+    def deserialize_data(self):
+        if self.is_data_exist():
+            f = open(self.datapath, 'rb')
+            data = pickle.load(f)
+            f.close()
+            return data
+        else:
+            data = CaseDate()
+            return data
+
+    def find_data(self, yuv_info, encode_name):
+        if len(self.data.case) == 0:
+            self.build_case(yuv_info)
+            self.build_encode(encode_name)
+            return False, None
+        for pic in self.data.case:
+            if self.is_have_yuv(yuv_info):
+                if self.is_have_encode(encode_name):
+                    return True, pic.group[encode_name]
+        return False, None
+
+    def is_have_yuv(self, yuv_info):
+        yuv_contain = [pic.yuv_info for pic in self.data.case]
+        if yuv_info in yuv_contain:
+            self.temp_case = self.data.case[yuv_contain.index(yuv_info)]
+            return True
+        else:
+            self.build_case(yuv_info)
+            return False
+
+    def is_have_encode(self, encode_name):
+        if encode_name in self.temp_case.group.keys():
+            return True
+        else:
+            self.build_encode(encode_name)
+            return False
+
+    def build_encode(self, encode_name):
+        self.temp_case.group[encode_name] = []
+        self.temp_case.group_bdrate[encode_name] = []
+
+    def build_case(self, yuv_info):
+        line_pool = LineContain()
+        line_pool.set_data_type(yuv_info)
+        self.temp_case = line_pool
+        self.data.case.append(line_pool)
+
+    def add_data(self, line, encode_name):
+        self.temp_case.group[encode_name].append(line)
+
+    def serialize_date(self):
+        f = open(self.datapath, 'wb')
+        pickle.dump(self.data, f)
+        f.close()
--- Data_struct.py
+++ Data_struct.py
@@ -2,20 +2,8 @@ import re
 import csv
 import subprocess
 import time
-import os
 from ycbcr import YCbCr
 import bjontegaard_metric as BD
-import OptionDictionary as option
-
-
-def decode(codec_name, bit_stream, yuv):
-    if codec_name == '265':
-        os.chdir(option.exec_path['HM'])
-        arg = option.decode_dict[codec_name] % (bit_stream, yuv)
-        p = subprocess.Popen(arg, shell=True, stdout=subprocess.PIPE)
-        info = p.communicate()
-    else:
-        print "we are not ready for 264"
 
 
 class Line:
@@ -285,59 +273,3 @@ class CaseDate:
                     file_contain.append(p)
             for l in file_contain:
                 writer.writerow(l)
-
-
-class ProEnv:
-    def __init__(self, p=None, codec_index=None, output=None, yuv=None, qp=None, mode=None):
-        self.progress = p
-        self.codec_index = codec_index
-        self.output = output
-        self.yuv = yuv
-        self.qp = qp
-        self.mode = mode
-
-
-class Pipeline:
-    def __init__(self, line):
-        self.pro = []
-        self.line = line
-        self.drop_tag = False
-
-    def push_pro(self, pro):
-        self.pro.append(pro)
-
-    def pop_pro_hm(self):
-        for pro in self.pro:
-            info = pro.progress.communicate()
-            decode(pro.codec_index[3], pro.output, pro.yuv)
-            self.line.add_info(info, pro.codec_index)
-            self.line.add_output(pro.yuv)
-            self.line.add_qp(pro.qp)
-        self.line.get_psnr(self.line)
-        return self.line
-
-    def pop_pro_other(self):
-        for pro in self.pro:
-            info = pro.progress.communicate()
-            if len(self.line.check_info(info)) is 0:
-                self.drop_tag = True
-                break
-            decode(pro.codec_index[3], pro.output, pro.yuv)
-            self.line.add_info(info, pro.codec_index)
-            self.line.add_output(pro.yuv)
-            self.line.add_qp(pro.qp)
-        if self.drop_tag:
-            return None
-        else:
-            self.line.get_psnr(self.line)
-            return self.line
-
-    def clear(self):
-        if not self.drop_tag:
-            for pro in self.pro:
-                os.remove(pro.yuv)
-                os.remove(pro.output)
-
-    def security(self):
-        for pro in self.pro:
-            pro.progress.terminate()

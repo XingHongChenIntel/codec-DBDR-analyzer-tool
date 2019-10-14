1b195605ea1fe8b284d4d3f96618efe0cf93a8fe
chandlerbingbing <327269655@qq.com>
2019-10-14 20:37:30 +0800

    save configure
    Signed-off-by: chandlerbingbing <327269655@qq.com>

--- OptionDictionary.py
+++ OptionDictionary.py
@@ -14,18 +14,25 @@ exec_path = {
     'svt': '/home/cxh/SVT-HEVC/Bin/Release/',
     'AV1': '/home/cxh/code/aom/cxhbuild/',
     'JM': '/home/cxh/code/JM/bin/',
-    'x264': '/home/cxh/code/x264/'
+    'x264': '/home/cxh/code/x264/',
+    'SvtAv1': '/home/cxh/code/SVT-AV1/Bin/Release/'
 }
 
 # test encoder and command line. it will be  QP, command line, encoder name, codec name, instance name,
 # is read from database(you can use 'read' or 'execute')
 # every time when you add new encode ,you should also add them to exec_path and codec_dict
-codec = [[[25, 29, 34, 38], './TAppEncoderStatic -c %s -fr 60', 'HM', '265', 't3', 'reae', 'baseline'],
+codec = [#[[25, 29, 34, 38], './TAppEncoderStatic -c %s -fr 60', 'HM', '265', 't3', 'reae', 'baseline'],
         # [[29, 35, 42, 48], './x265 --fps 60', 'x265', '265', 'test265', 're', 'None'],
-        [[22, 28, 34, 40], './SvtHevcEncApp', 'svt', '265', 'testmode', 're', 'None'],
-        [[29, 35, 42, 48], './SvtHevcEncApp', 'svt', '265', 'test', 're', 'None'],
+        # [[22, 28, 34, 40], './SvtHevcEncApp', 'svt', '265', 'testmode', 're', 'baseline'],
+        # [[29, 35, 42, 48], './SvtHevcEncApp', 'svt', '265', 'test', 're', 'None'],
         #[[22, 28, 34, 40], './aomenc --threads=64', 'AV1', 'AV1', 'testmode', 'read'],
         #[[22, 28, 34, 40], './x264', 'x264', '264', 'testmode', 'read'],
+        [[20, 32, 43, 55], './SvtAv1EncApp -n 60 -intra-period 119 -c /home/cxh/code/SVT-AV1/Config/Sample.cfg \
+         -lad 119', 'SvtAv1', 'AV1', 'test', 're', 'None'],
+        [[20, 32, 43, 55], './aomenc --codec=av1 --psnr --verbose --passes=1 --threads=40 --i420 --profile=0 \
+        --frame-parallel=0 --tile-columns=0 --test-decode=fatal --kf-min-dist=120 --kf-max-dist=120 --end-usage=q \
+        --lag-in-frames=25  --auto-alt-ref=2 --aq-mode=0 --bit-depth=8 --fps=30000/1000', \
+         'AV1', 'AV1', 'testmode', 're', 'baseline'],
 ]
 
 # choose the test sample type, for example 360p 720p 1080p 2k 4k 8k
@@ -39,15 +46,17 @@ codec_dict = {
     'HM': '-i %s -wdt %s -hgt %s -q %s -b %s',
     'x265': '--input %s --input-res %sx%s --input-depth %s -q %s -p %s -o %s',
     'svt': '-i %s -w %s -h %s -bit-depth %s -q %s -encMode %s -b %s -color-format %s',
-    'AV1': ' %s -w %s -h %s -b %s --min-q=%s --cpu-used=%s -o %s',
+    'AV1': ' %s --width=%s --height=%s --input-bit-depth=%s --cq-level=%s --cpu-used=%s -o %s',
     'x264': ' %s --input-res %sx%s --input-depth %s -q %s --preset %s -o %s',
+    'SvtAv1': '-i %s -w %s -h %s -bit-depth %s -q %s -enc-mode %s -b %s'
 }
 
 # decoder path
 decode_dict = {
     '265': './TAppDecoderStatic -b %s -o %s',
-    'AV1': './aomdec %s -o %s',
+    'AV1': './aomdec --codec=av1 %s -o %s --i420',
     '264': './ldecod.exe -i %s -o %s',
+    'SvtAv1': './aomdec --codec=av1 %s -o %s --i420',
 }
 
 # encode yuv data save path
@@ -59,4 +68,4 @@ calculate_data = '%s/data.csv' % proxy
 # the path to save the plot picture
 plot_path = proxy
 
-TestTag = True
+TestTag = False

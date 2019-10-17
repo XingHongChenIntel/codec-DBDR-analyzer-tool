fd5c0b2fa8e9b3570a5e8bbb47f09e135afc72b8
chandlerbingbing <327269655@qq.com>
2019-10-17 15:24:47 +0800

    save local configure
    Signed-off-by: chandlerbingbing <327269655@qq.com>

--- OptionDictionary.py
+++ OptionDictionary.py
@@ -11,7 +11,7 @@ TestSequencePath = '/home/cxh/code/codec-DBDR-analyzer-tool/test.csv'
 exec_path = {
     'HM': '/home/cxh/code/HM-16.1/bin/',
     'x265': '/home/cxh/code/x265/build/linux/',
-    'svt': '/home/cxh/SVT-HEVC/Bin/Release/',
+    'svt': '/home/cxh//code/svt-hevc-master-039366e/BDRate-patch/encoders/039366e/',
     'AV1': '/home/cxh/code/aom/cxhbuild/',
     'JM': '/home/cxh/code/JM/bin/',
     'x264': '/home/cxh/code/x264/',
@@ -24,7 +24,8 @@ exec_path = {
 codec = [#[[25, 29, 34, 38], './TAppEncoderStatic -c %s -fr 60 -f 60', 'HM', '265', 't3', 'reae', 'baseline'],
          # [[25, 29, 34, 38], './TAppEncoderStatic -c %s -fr 60', 'HM', '265', 't3', 'reae', 'None'],
         # [[29, 35, 42, 48], './x265 --fps 60', 'x265', '265', 'test265', 're', 'None'],
-        [[22, 28, 34, 40], './SvtHevcEncApp', 'svt', '265', 'testmode', 're', 'baseline'],
+        # [[22, 28, 34, 40], './SvtHevcEncApp', 'svt', '265', 'testmode', 're', 'baseline'],
+        [[22, 27, 32, 37], './SvtHevcEncApp -fps 60 -intra-period 55 ', 'svt', '265', 'vailiate', 're', 'baseline'],
         # [[22, 28, 34, 40], './SvtHevcEncApp', 'svt', '265', 'test', 're', 'None'],
         # [[22, 28, 34, 40], './SvtHevcEncApp', 'svt', '265', 'tes', 're', 'None'],
         # [[22, 28, 34, 40], './aomenc --threads=64', 'AV1', 'AV1', 'testmo', 'rea', 'baseline'],
@@ -43,10 +44,10 @@ codec = [#[[25, 29, 34, 38], './TAppEncoderStatic -c %s -fr 60 -f 60', 'HM', '26
 
 # choose the test sample type, for example 360p 720p 1080p 2K 4K 8K
 # if you want to test all sample use 'all
-Test_data_type = '4K'
+Test_data_type = 'all'
 Multi_process = 1
 # encode mode for different encoder
-mode = [0,]# 1, 2, 3, ]# 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+mode = [0,] # 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 # encoder command line parameter about yuv info, if test new encoder, need to add information here
 codec_dict = {
     'HM': '-i %s -wdt %s -hgt %s -q %s -b %s',
